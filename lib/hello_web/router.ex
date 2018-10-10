972c77703030a1eac709351ce6b1392de26848ac
jwkyle <jim@jwkyle.com>
2018-10-09 23:33:43 -0500

    add example code for plugs and passthroughs

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -1,3 +1,25 @@
+# Plug for demo purposes only
+# defmodule BackgroundJob.Plug do
+#   def init(opts), do: opts
+#   def call(conn, opts) do
+#     conn
+#     |> Plug.Conn.assign(:name, Keyword.get(opts, :name, "Background Job"))
+#     |> BackgroundJob.Router.call(opts)
+#   end
+# end
+
+# defmodule BackgroundJob.Router do
+#   use Plug.Router
+
+#   plug :match
+#   plug :dispatch
+
+#   get "/", do: send_resp(conn, 200, "Welcome to #{conn.assigns.name}")
+#   get "/active", do: send_resp(conn, 200, "5 Active Jobs")
+#   get "/pending", do: send_resp(conn, 200, "3 Pending Jobs")
+#   match _, do: send_resp(conn, 404, "Not found")
+# end
+
 defmodule HelloWeb.Router do
   use HelloWeb, :router
 
@@ -23,6 +45,12 @@ defmodule HelloWeb.Router do
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
   end
+
+  # Used to illustrate pipethrough
+  # scope "/" do
+  #   pipe_through [:authenticate_user, :ensure_admin]
+  #   forward "/jobs", BackgroundJob.Plug, name: "Hello Phoenix!"
+  # end
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloWeb do
