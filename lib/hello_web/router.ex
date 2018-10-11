193f3a95e12afaf829eabccdce88cae8c42186c7
jwkyle <jim@jwkyle.com>
2018-10-11 01:20:02 -0500

    edit router to comment out customer pipe and pipe_through examples

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -31,10 +31,11 @@ defmodule HelloWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :review_checks do
-    plug :ensure_authenticated_user
-    plug :ensure_user_owns_review
-  end
+  # Custom Pipe Example
+  # pipeline :review_checks do
+  #   plug :ensure_authenticated_user
+  #   plug :ensure_user_owns_review
+  # end
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -49,16 +50,17 @@ defmodule HelloWeb.Router do
     end
     # resources "/posts", PostController, only: [:index, :show]
     resources "/comments", PostController, except: [:delete]
-    # resources "/reviews", ReviewController
+    resources "/reviews", ReviewController
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
   end
 
-  scope "/reviews", HelloWeb do
-    pipe_through :review_checks
+  # Custom pipe_through example
+  # scope "/reviews", HelloWeb do
+  #   pipe_through :review_checks
 
-    resources "/", ReviewController
-  end
+  #   resources "/", ReviewController
+  # end
 
   scope "/admin", HelloWeb.Admin, as: :admin do
     pipe_through :browser
